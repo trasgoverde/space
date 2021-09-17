b8ae04200e43520e8cc406e32fab4e0e07a6b68e
Ignacio Souto <ignacio.souto@blocknitive.com>
2021-09-17 10:42:20 +0200

    New files :red: :red: :red:

---
+++ truffle.js
@@ -0,0 +1,26 @@
+//Add these after react c=>
+require('babel-register');
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ganache: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    solc: {
+      // default is 0.5.16
+      version: ">=0.5.16 <0.9.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
