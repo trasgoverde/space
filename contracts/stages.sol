d52fddad2698f97d6a3594a629a9272c9d8ddecd
Ign <ign.souto@gmail.com>
2021-09-29 17:49:04 +0200

    Update stages.sol

--- contracts/stages.sol
+++ contracts/stages.sol
@@ -50,9 +50,9 @@ contract StateMachine {
     function setStages (uint256 PreIco, uint256 ico, uint256 PostIco) public onlyOwner {
         if(uint256(Stages.PreIco) == _stage) {
             _stage = Stages.PreIco;
-        else (uint256(Stages.ico)) == _stage {
+        else if (uint256(Stages.ico)) == _stage {
              _stage = Stages.ico;
-        else (uint256(Stages.PostIco)) == _stage {
+        else if (uint256(Stages.PostIco) == _stage {
              _stage = Stages.PostIco;
         }     
         
