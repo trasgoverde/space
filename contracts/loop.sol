e81091322d36ddb7f7b75c57679b9325f480e1c7
Ignacio <ign.souto@gmail.com>
2021-09-29 19:39:38 +0200

    Ownable added error line 1067 (onlyOwner) 🤬  🤬

--- contracts/loop.sol
+++ contracts/loop.sol
@@ -1,4 +1,5 @@
 // SPDX-Licence-Identifier: UNLICENSED
+
 pragma solidity 0.8.0;
 
 /*
@@ -352,6 +353,80 @@ library SafeMath {
             // solhint-disable-next-line max-line-length
             require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
         }
+    }
+}
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Returns true if the caller is the current owner.
+     */
+    function isOwner() public view returns (bool) {
+        return _msgSender() == _owner;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
     }
 }
 
@@ -983,13 +1058,13 @@ contract PrivateSpaceCrowdsale is Crowdsale, TimedCrowdsale {
     {
         return contributions[_beneficiary];
     }
-
+  
     /**
     * @dev Allows admin to update the crowdsale stage
     * @param _stage Crowdsale stage
     */
     
-    function setCrowdSaleStage(uint _stage) public onlyAdmin {
+    function setCrowdsaleStage(uint _stage) public onlyOwner {
       if (uint(CrowdsaleStage.PreICO) == _stage) {
           stage = CrowdsaleStage.PreICO;
       } else if (uint(CrowdsaleStage.ICO) == _stage) {
@@ -1005,6 +1080,7 @@ contract PrivateSpaceCrowdsale is Crowdsale, TimedCrowdsale {
       } else if (stage == CrowdStage.PostICO) {
           rate = 50000;
       }
+   
     }
     
 
